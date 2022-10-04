c47af0acb353b39d5f7c3cd0da206a3d922756cd
JKosonen <jussi.k.kosonen@gmail.com>
2022-10-04 19:28:41 +0200

    projekti luotu

---
+++ src/main/java/com/example/RuokalistaApp/RuokalistaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.RuokalistaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuokalistaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuokalistaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RuokalistaApp/RuokalistaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RuokalistaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuokalistaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
